1b8b894baccc2c6047f14778d324aacb410bcb69
Harrison <167173157+DrSilverDragon@users.noreply.github.com>
2025-05-14 12:03:44 +1000

    #89 Updated the inventory system (#90)
    * #89 Changed inventory to use items instead of strings.
    
    Inventory now uses a list of items, where item is a struct.
    
    Allowed the player to collect "key items".
    
    * #89 Fixed some unity scene data.
    
    * #89 Added the ability to have item added to the inventory at level start.

---
+++ Game Prototype/Prototype/Assets/Scripts/Data/item.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+/// <summary>
+/// A basic struct that stores item data.
+/// </summary>
+[System.Serializable]
+public struct item
+{
+    public string id;
+    public string name;
+    public bool isKeyItem;
+    public bool isConsumed;
+
+    public item(string id, string name, bool isKeyItem, bool isConsumed)
+    {
+        this.id = id;
+        this.name = name;
+        this.isKeyItem = isKeyItem;
+        this.isConsumed = isConsumed;
+    } 
+}
