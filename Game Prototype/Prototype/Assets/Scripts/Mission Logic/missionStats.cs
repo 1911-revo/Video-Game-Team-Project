a49e73abeaf52a9216b74ef25affe2e322274d9a
Harrison <167173157+DrSilverDragon@users.noreply.github.com>
2025-05-14 14:43:15 +1000

    #77 Mission logic can now be filled out and completed.
    Added triggers that can run any public void.
    Added a completion screen that shows the time in level.
    Items are now properly collectable.
    
    Bugs:
    Currently inventory animation doesn't work, will be fixed when we combine all work into a test level.

---
+++ Game Prototype/Prototype/Assets/Scripts/Mission Logic/missionStats.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class missionStats : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timestamp;
+    [SerializeField] public SavedMissionStats stats;
+
+    float timer = 0f;
+
+    /// <summary>
+    /// Keeps track of time spent in level
+    /// </summary>
+    void Update()
+    {
+        timer += Time.deltaTime;
+        int hours = Mathf.FloorToInt(timer / 3600f);
+        int minutes = Mathf.FloorToInt((timer % 3600f) / 60f);
+        int seconds = Mathf.FloorToInt(timer % 60f);
+        int fakeframes = Mathf.FloorToInt((timer * 30) % 30);
+
+        string time = string.Format("{0:00}:{1:00}:{2:00}:{3:00}", hours, minutes, seconds, fakeframes);
+
+        timestamp.text = time;
+
+        stats.time = time;
+    }
+}
